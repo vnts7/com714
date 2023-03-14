b9a15a6f875292edfc0d69ae3be250aee869d206
Tai Nguyen <vannguyents@gmail.com>
2023-03-14 20:39:03 +0700

    add get_all_lines function

--- board.py
+++ board.py
@@ -9,6 +9,35 @@ class Board:
 
     def set_cell(self, row, col, value):
         self.grid[row][col] = value
+    
+    # get all lines 5 cells and 6 cells
+    def get_all_lines(self):
+        grid = self.grid
+        n = len(grid)
+        lines = []
+        # horizontal lines
+        for i in range(n):
+            for j in range(n-4):
+                lines.append(grid[i][j:j+5])
+            for j in range(n-5):
+                lines.append(grid[i][j:j+6])
+
+        # vertical lines
+        for i in range(n):
+            for j in range(n-4):
+                lines.append([grid[k][i] for k in range(j, j+5)])
+            for j in range(n-5):
+                lines.append([grid[k][i] for k in range(j, j+6)])
+        # diagonal lines
+        for i in range(n-4):
+            for j in range(n-4):
+                lines.append([grid[i+k][j+k] for k in range(5)])
+                lines.append([grid[i+k][j+4-k] for k in range(5)])
+        for i in range(n-5):
+            for j in range(n-5):
+                lines.append([grid[i+k][j+k] for k in range(6)])
+                lines.append([grid[i+k][j+5-k] for k in range(6)])
+        return lines
 
     def is_empty_cell(self, row, col):
         return self.grid[row][col] == None
